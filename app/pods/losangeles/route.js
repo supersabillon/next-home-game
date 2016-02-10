a1b19ba289d0cd18299c9f7ae34f6b54da72b4f2
supersabillon <joel@sabillon.net>
2016-02-09 22:02:14 -0800

    deleted all the routes of each team and created one for teams, also added model

--- app/pods/losangeles/route.js
+++ app/pods/losangeles/route.js
@@ -2,7 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    
+    var teams = {
+      data: [{
+        "name": "Galaxy"
+      },
+      {
+        "name": "Kings"
+      },
+      {
+        "name": "Lakers"
+      },
+      {
+        "name": "Clippers"
+      },
+      {
+        "name": "Dodgers"
+      },
+      {
+        "name": "Rams"
+      }]
+    };
+
+    return teams;
   }
 
 });
--- app/router.js
+++ app/router.js
@@ -7,10 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('losangeles', { path: '/LosAngeles' }, function() {
-    this.route('lagalaxy', { path: '/Galaxy' });
-    this.route('lakers', { path: '/Lakers'} );
-    this.route('clippers', { path: '/Clippers'} );
-    this.route('kings', { path: '/Kings'} );
+    this.route('teams');
   });
 });
 
