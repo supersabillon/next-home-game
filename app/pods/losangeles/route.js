b9391c48fbc9083952dcd5887166e21cee0d847c
supersabillon <joel@sabillon.net>
2016-02-12 21:39:39 -0800

    added schedule for Galaxy up to May

--- app/pods/losangeles/route.js
+++ app/pods/losangeles/route.js
@@ -4,7 +4,44 @@ export default Ember.Route.extend({
   model() {
     var teams = {
       data: [{
-        "name": "Galaxy"
+        "name": "Galaxy",
+        "schedule": [
+            {
+                team: "Santos Laguna",
+                date: "2016-02-24 19",
+                competition: "CCL"
+            },
+            {
+                team: "DC United",
+                date: "2016-03-06 19",
+                competition: "MLS"
+            },
+            {
+                team: "SJ Earthquakes",
+                date: "2016-03-19 19:30",
+                competition: "MLS"
+            },
+            {
+                team: "Portland Timbers",
+                date: "2016-04-10 18:30",
+                competition: "MLS"
+            },
+            {
+                team: "Real Salt Lake",
+                date: "2016-04-23 19:30",
+                competition: "MLS"
+            },
+            {
+                team: "NE Revolution",
+                date: "2016-05-08 12:30",
+                competition: "MLS"
+            },
+            {
+                team: "SJ Earthquakes",
+                date: "2016-05-22 16",
+                competition: "MLS"
+            }
+        ]
       },
       {
         "name": "Kings"
