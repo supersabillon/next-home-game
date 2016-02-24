9e9657fd37c0133d9186d7718295533f75d91814
supersabillon <joel@sabillon.net>
2016-02-23 23:01:19 -0800

    made progress in next game component

--- app/pods/components/next-game/component.js
+++ app/pods/components/next-game/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  opponent: Ember.computed('schedule', function() {
+    var schedule = this.get('schedule');
+      console.log('schedule is ' + schedule[0].team);
+    return 'test';
+  })
 });
