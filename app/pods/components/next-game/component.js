ddb4c8bcb51c038f7888a69690ac46336df6c6f3
supersabillon <joel@sabillon.net>
2016-02-27 21:56:11 -0800

    updating ember-cli

--- app/pods/components/next-game/component.js
+++ app/pods/components/next-game/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   opponent: Ember.computed('schedule', function() {
     var schedule = this.get('schedule');
-      console.log('schedule is ' + schedule[0].team);
+      console.log('schedule is ' + schedule);
     return 'test';
   })
 });
