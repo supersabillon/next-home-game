cdc3ccdc10c88efd74688760decca78b12fb32f1
supersabillon <joel@sabillon.net>
2016-03-01 22:10:02 -0800

    adding moment to global namespace

--- app/pods/components/next-game/component.js
+++ app/pods/components/next-game/component.js
@@ -1,8 +1,16 @@
+/*global moment*/
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
   opponent: Ember.computed('schedule', function() {
-    var schedule = this.get('schedule');
+    let schedule = this.get('schedule');
+    let gameDates = schedule.map(function(item, index, enumerable) {
+      console.log(moment(item.date));
+    });
+    let now = moment();
+        
+    console.log(now);
     return schedule[0].team;
   })
 });
