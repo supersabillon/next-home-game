4f3c31100daefb9ece193d2ed2918703ba95fcd7
supersabillon <joel@sabillon.net>
2016-03-03 22:02:16 -0800

    adding helper function to get lowerst number of array

--- app/pods/components/next-game/component.js
+++ app/pods/components/next-game/component.js
@@ -4,13 +4,28 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   opponent: Ember.computed('schedule', function() {
+    const NOW = moment();
     let schedule = this.get('schedule');
-    let gameDates = schedule.map(function(item, index, enumerable) {
-      console.log(moment(item.date));
+    let nextGame = schedule.map(function(item) {
+      return moment(item.date).diff(NOW, 'days');
     });
-    let now = moment();
-        
-    console.log(now);
+    
+    console.log(this.getIndexOfLowest(nextGame));
+
     return schedule[0].team;
-  })
+  }),
+  
+  getIndexOfLowest(arr) {
+    let min = arr[0];
+    let minIndex = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            minIndex = i;
+            min = arr[i];
+        }
+    }
+    
+    return minIndex;
+  }
 });
