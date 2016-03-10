f55e63e50ca5db73f94ab2d0f3d57160ace44b4c
supersabillon <joel@sabillon.net>
2016-03-09 22:26:27 -0800

    adding moment format to game-meta

--- app/pods/components/game-meta/component.js
+++ app/pods/components/game-meta/component.js
@@ -1,8 +1,10 @@
+/*global moment*/
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
   time: Ember.computed('opponent', function () {
-    return this.get('opponent.date');
+    return moment(this.get('opponent.date')).format('dddd MMMM Do, h:mm a');
   })
 
 });
