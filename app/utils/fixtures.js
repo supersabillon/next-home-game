40439f068866d490a721db69033f0a49afaaed64
supersabillon <joel@sabillon.net>
2016-03-13 23:37:50 -0700

    adding schedule for dodgers

--- app/utils/fixtures.js
+++ app/utils/fixtures.js
@@ -152,6 +152,419 @@ export var data = [{
       }
     ]
   },
+  {
+    name: 'Dodgers',
+    address: {
+      name: 'Dodger Stadium',
+      street: '1000 Elysian Park ave',
+      city: 'Los Angeles',
+      state: 'CA',
+      zip: 90012
+    },
+    schedule: [
+      {
+        team: 'Arizona Diamondbacks',
+        date: '2016-04-12 13:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Arizona Diamondbacks',
+        date: '2016-04-13 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Arizona Diamondbacks',
+        date: '2016-04-14 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Francisco Giants',
+        date: '2016-04-15 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Francisco Giants',
+        date: '2016-04-16 18:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Francisco Giants',
+        date: '2016-04-17 17:05',
+        competition: 'MLB'
+      },
+      {
+        team: 'Miami Marlins',
+        date: '2016-04-25 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Miami Marlins',
+        date: '2016-04-26 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Miami Marlins',
+        date: '2016-04-27 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Miami Marlins',
+        date: '2016-04-28 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Diego Padres',
+        date: '2016-04-29 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Diego Padres',
+        date: '2016-04-30 18:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Diego Padres',
+        date: '2016-05-01 13:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'New York Mets',
+        date: '2016-05-09 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'New York Mets',
+        date: '2016-05-10 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'New York Mets',
+        date: '2016-05-11 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'New York Mets',
+        date: '2016-05-12 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'St. Louis Cardinals',
+        date: '2016-05-13 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'St. Louis Cardinals',
+        date: '2016-05-14 18:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'St. Louis Cardinals',
+        date: '2016-05-15 17:05',
+        competition: 'MLB'
+      },
+      {
+        team: 'Los Angeles Angels',
+        date: '2016-05-16 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Los Angeles Angels',
+        date: '2016-05-17 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Cincinnati Reds',
+        date: '2016-05-23 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Cincinnati Reds',
+        date: '2016-05-24 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Cincinnati Reds',
+        date: '2016-05-25 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Atlanta Braves',
+        date: '2016-06-03 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Atlanta Braves',
+        date: '2016-06-04 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Atlanta Braves',
+        date: '2016-06-05 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Colorado Rockies',
+        date: '2016-06-06 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Colorado Rockies',
+        date: '2016-06-07 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Colorado Rockies',
+        date: '2016-06-08 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Milwaukee Brewers',
+        date: '2016-06-16 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Milwaukee Brewers',
+        date: '2016-06-17 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Milwaukee Brewers',
+        date: '2016-06-18 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Washington Nationals',
+        date: '2016-06-20 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Washington Nationals',
+        date: '2016-06-21 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Washington Nationals',
+        date: '2016-06-22 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Colorado Rockies',
+        date: '2016-07-01 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Colorado Rockies',
+        date: '2016-07-02 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Colorado Rockies',
+        date: '2016-07-03 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Baltimore Orioles',
+        date: '2016-07-04 18:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Baltimore Orioles',
+        date: '2016-07-05 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Baltimore Orioles',
+        date: '2016-07-06 12:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Diego Padres',
+        date: '2016-07-07 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Diego Padres',
+        date: '2016-07-08 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Diego Padres',
+        date: '2016-07-09 16:15',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Diego Padres',
+        date: '2016-07-10 13:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Tampa Bay Rays',
+        date: '2016-07-26 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Tampa Bay Rays',
+        date: '2016-07-27 12:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Arizona Diamondbacks',
+        date: '2016-07-29 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Arizona Diamondbacks',
+        date: '2016-07-30 18:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Arizona Diamondbacks',
+        date: '2016-07-31 13:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Boston Red Sox',
+        date: '2016-08-05 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Boston Red Sox',
+        date: '2016-08-06 13:05',
+        competition: 'MLB'
+      },
+      {
+        team: 'Boston Red Sox',
+        date: '2016-08-07 16:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Philadelphia Phillies',
+        date: '2016-08-08 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Philadelphia Phillies',
+        date: '2016-08-09 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Philadelphia Phillies',
+        date: '2016-08-10 12:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Pittsburgh Pirates',
+        date: '2016-08-12 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Pittsburgh Pirates',
+        date: '2016-08-13 13:05',
+        competition: 'MLB'
+      },
+      {
+        team: 'Pittsburgh Pirates',
+        date: '2016-08-14 13:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Francisco Giants',
+        date: '2016-08-23 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Francisco Giants',
+        date: '2016-08-24 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Francisco Giants',
+        date: '2016-08-25 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Chicago Cubs',
+        date: '2016-08-26 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Chicago Cubs',
+        date: '2016-08-27 13:05',
+        competition: 'MLB'
+      },
+      {
+        team: 'Chicago Cubs',
+        date: '2016-08-28 13:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Diego Padres',
+        date: '2016-09-02 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Diego Padres',
+        date: '2016-09-03 18:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Diego Padres',
+        date: '2016-09-04 13:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Arizona Diamondbacks',
+        date: '2016-09-05 17:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Arizona Diamondbacks',
+        date: '2016-09-06 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Arizona Diamondbacks',
+        date: '2016-09-07 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Francisco Giants',
+        date: '2016-09-19 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Francisco Giants',
+        date: '2016-09-20 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'San Francisco Giants',
+        date: '2016-09-21 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Colorado Rockies',
+        date: '2016-09-22 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Colorado Rockies',
+        date: '2016-09-23 19:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Colorado Rockies',
+        date: '2016-09-24 18:10',
+        competition: 'MLB'
+      },
+      {
+        team: 'Colorado Rockies',
+        date: '2016-09-25 13:10',
+        competition: 'MLB'
+      }
+    ]
+
+  },
   // {
   //   "name": "Kings"
   // },
@@ -161,9 +574,7 @@ export var data = [{
   // {
   //   "name": "Clippers"
   // },
-  // {
-  //   "name": "Dodgers"
-  // },
+
   // {
   //   "name": "Rams"
   // }
