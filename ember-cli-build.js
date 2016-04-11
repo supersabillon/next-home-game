657d7f970e934f1ad04e60a176f779325db51d27
supersabillon <joel@sabillon.net>
2016-04-10 23:20:16 -0700

    adding lodash

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -24,6 +24,7 @@ module.exports = function(defaults) {
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  app.import('bower_components/lodash/lodash.js');
   
   return app.toTree();
 };
