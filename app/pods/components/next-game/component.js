96510597ea23e073198d341cde3f704cd158ca0d
supersabillon <joel@sabillon.net>
2016-04-12 22:27:12 -0700

    passing home team and opponent info to template, just need to sort now

--- app/pods/components/next-game/component.js
+++ app/pods/components/next-game/component.js
@@ -4,39 +4,31 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  opponents: Ember.computed(function() {
+  fixtures: Ember.computed(function() {
     const NOW = moment();
     let self = this;
 
     let data = this.get('model').map(function(team) {
+      let obj = {};
 
+      obj.name = team.name;
+      obj.address = team.address;
+      
       if(team.schedule) {
         let games = team.schedule.filter(function(element, index) {
           return moment(element.date).diff(NOW, 'days') >= 0;
         });
-        return games[self.getIndexOfLowest(games)];
+        obj.opponent = games[0];
        };
-
+      
+      return obj;
     });
 
-    let sorted = data.sort(function(a,b) {
-      return moment(a.date) - moment(b.date);
-    });
+    // // sort by date
+    // let sorted = data.sort(function(a,b) {
+    //   return moment(a.date) - moment(b.date);
+    // });
 
-    return sorted;
-  }),
-
-  getIndexOfLowest(arr) {
-    let min = arr[0];
-    let minIndex = 0;
-
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] < min) {
-        minIndex = i;
-        min = arr[i];
-      }
-    }
-
-    return minIndex;
-  }
+    return data;
+  })
 });
